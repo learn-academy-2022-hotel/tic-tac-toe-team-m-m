195474d23bf0aef1af2d5acd08e4ca6b48a0f191
MLMunoz85 <matthew.l.munoz@gmail.com>
2023-01-03 13:52:45 -0800

    user story #2 completed

--- src/App.js
+++ src/App.js
@@ -5,13 +5,24 @@ import './App.css'
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
 
+const handleGamePlay = (clickedSquare) => {
+  let updateSquare = [...squares]
+  updateSquare[clickedSquare] = "X"
+  setSquares(updateSquare)
+}
+
+
   return (
     <>
       <h1>Tic Tac Toe</h1>
       <div className='gameBoard'>
-        {squares.map(() => {
+        {squares.map((square, index) => {
           return (
-            <Square />
+            <Square
+            square={square}
+            index={index}
+            handleGamePlay={handleGamePlay}
+            />
           )
         }
       )}
